33d731349e9cd760f783a253ed005714a6df6a5c
Izaide Tenutes <izaidedmngs@gmail.com>
2023-01-19 15:54:17 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+          public static void main(String[] args) {
+
+
+        Scanner leitor = new Scanner(System.in);
+
+        int idade1 = leitor.nextInt();
+        int idade2 = leitor.nextInt();
+        int idade3 = leitor.nextInt();
+
+        double mediaIdade = (idade1 + idade2 + idade3) / 3;
+
+        if (mediaIdade <= 25) {
+            System.out.println("Jovem!");
+        } else if (mediaIdade <= 60) {
+            System.out.println("Adulta!");
+        } else   {
+            System.out.println("Idosa!");
     }
-}
+}}
